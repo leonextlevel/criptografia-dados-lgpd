f0032729bf71844b879b23bd4168efc18cb5d44a
matheus <matheusrothstein1@gmail.com>
2020-05-23 10:31:07 -0300

    melhoria realizada no cadastro de usuários na main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,17 @@ from crypt.gererate_data_crypt import InsertByPeewee
 if __name__ == '__main__':
     # Para inserir dados criptografados, seguir o exemplo:
     user = InsertByPeewee(models.Usuario)
+
+    user_name = input('Por favor insira o nome do usuário: \n')
+    user_email = input('Por favor insira o email do usuário: \n')
+    user_password = input('Por favor insira a senha de acesso: \n')
+    user_nascimento = input('Por favor insira a data de nascimento no formato dd-mm-aaaa: \n')
+    user_cep = input('Por favor insira o cep do usuário: \n')
+
+
     user.create(
-        data={'cep': '33333-333', 'data_nascimento': '02-01-2000', 'password': '12345'},
-        data_cripty={'nome': 'Teste', 'email': 'teste@email.com'}
+        data={'cep': user_cep, 'data_nascimento': user_nascimento, 'password': user_password},
+        data_cripty={'nome': user_name, 'email': user_email}
     )
+
+    
